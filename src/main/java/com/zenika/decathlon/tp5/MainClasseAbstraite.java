f646a7751cd2c948c7d49e1cd4956e7bcaed7a85
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-03-14 17:32:13 +0100

    Initial commit

---
+++ src/main/java/com/zenika/decathlon/tp5/MainClasseAbstraite.java
@@ -0,0 +1,8 @@
+package com.zenika.decathlon.tp5;
+
+public class MainClasseAbstraite {
+	public static void main(String[] args) {
+		ClasseConcrete classeConcrete = new ClasseConcrete();
+		classeConcrete.methodeTemplate();
+	}
+}
